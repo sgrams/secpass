a63fac716bb94cbb7a58590c3cf856dedb5efd19
Stanisław Grams <sgrams@fmdx.pl>
2019-05-19 22:00:17 +0200

    [src] add new global defines

--- src/common/secpass.h
+++ src/common/secpass.h
@@ -7,7 +7,8 @@
 #ifndef _SECPASS_COMMON_H
 #define _SECPASS_COMMON_H
 
-#define SECPASS_VERSION "0.1-git"
-#define SECPASS_AUTHORS "Stanisław Grams <sgrams@fmdx.pl>"
+#define SECPASS_VERSION  "0.1-git"
+#define SECPASS_AUTHORS  "Stanisław Grams <sgrams@fmdx.pl>"
+#define SECPASS_HOMEPAGE "https://secpass.org/"
 
 #endif // _SECPASS_COMMON_H
