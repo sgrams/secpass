d6d70d7e4e396d4409d41b7cdececb552f4514c6
Stanisław Grams <sjg@fmdx.pl>
2019-05-27 23:16:08 +0200

    [src/core] add core enclave implementation and basic ECALL/OCALL definitions

---
+++ src/core/core.cc
@@ -0,0 +1,18 @@
+/* src/core/core.cc
+ * Stanisław Grams <sgrams@fmdx.pl>
+ *
+ * Contains implementations of functions
+ * accessing the enclave
+ *
+ * secpass.org
+ * © 2019
+ */
+#include <string>
+#include <map>
+#include <vector>
+
+#include "core.h"
+
+using namespace std;
+
+
