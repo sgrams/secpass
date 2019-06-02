94dcd86967c01767eef383f3e64994c55af35471
Stanisław Grams <sjg@fmdx.pl>
2019-06-02 10:09:37 +0200

    [src] try using SGX bridge

--- src/core/core.h
+++ src/core/core.h
@@ -13,6 +13,8 @@
 #include "../common/secpass.h"
 
 #include <sgx.h>
+
+using namespace std;
 // version of file format
 #define SECPASS_CORE_FILE_VERSION 0x01
 
@@ -32,43 +34,13 @@ typedef uint64_t core_status_t;
 typedef uint8_t version_t;
 
 // kdf_t: enum determining KDF function used
-typedef enum kdf { ARGON2, RESERVED } kdf_t;
+typedef enum kdf { ARGON2, KDF_RESERVED } kdf_t;
 
 // enc_t: enum determining encryption function used
-typedef enum enc { AES256GCM, RESERVED } enc_t;
+typedef enum enc { AES256GCM, ENC_RESERVED } enc_t;
 
 // iv_t: 8 bit variable containing init vector
 typedef uint8_t iv_t[32]; // 32 * 8 bit = 256 bit IV
 
-// file_t: structure containing both secret data and its' metadata
-typedef struct file {
-  version_t  version;
-  kdf_t      kdf;
-  iv_t       iv;
-  enc_t      enc;
-
-  database_t  db;
-  string      db_path;
-  string      db_key;
-} file_t;
-
-// database_t: structure-wrapper containing informations about entities
-typedef struct database {
-  uint64_t epoch_created;
-  uint64_t epoch_modified;
-  std::vector<entity_t> entities;
-} database_t;
-
-// entity_t: structure-wrapper for secret data and it's metadata
-typedef struct entity {
-  uint64_t id;
-
-  uint64_t expiry_time; // expiry time epoch
-  uint8_t  expired;
-
-  string   name;
-  secret_t secret;
-} entity_t;
-
-typedef string secret_t;
+typedef char * secret_t;
 #endif // _SECPASS_CORE_H
--- src/nctui/main.cc
+++ src/nctui/main.cc
@@ -11,9 +11,27 @@
 #include <unistd.h>
 
 #include "../common/secpass.h"
+#include "../core/core_u.h"
+
+#include "sgx_eid.h"
+#include "sgx_urts.h"
+
+sgx_enclave_id_t global_eid  = 0;
 
 // prototypes
 void print_help ();
+
+int initialize_enclave(void)
+{
+  sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+
+  ret = sgx_create_enclave("enclave.so", 1, NULL, NULL, &global_eid, NULL);
+  if (ret != SGX_SUCCESS) {
+    return -1;
+  }
+
+  return 0;
+}
 
 /*!
  * Main function
@@ -27,12 +45,18 @@ void print_help ();
 int
 main (int argc, char *argv[])
 {
+  int x = 0;
   int status = 0;
 
-  if (argc < 2) {
-    print_help ();
-    return status;
+  if (initialize_enclave () < 0) {
+    std::cerr << "dupa!\n";
   }
+  sgx_status_t rv = entry_test (global_eid, &x);
+  std::cout << x << std::endl;
+  if (rv != SGX_SUCCESS) {
+    std::cout << "noob" << std::endl;
+  }
+  std::cout << "wtf!" << std::endl;
 
   return status;
 }
@@ -45,7 +69,7 @@ main (int argc, char *argv[])
  * @author Stanisław Grams
  * @date   2019/05/03
  */
-void
+  void
 print_help ()
 {
   std::cout <<
