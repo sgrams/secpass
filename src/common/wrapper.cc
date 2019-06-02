1e36780992a83d58184aa7df48b3fce93b43d65c
Stanisław Grams <sjg@fmdx.pl>
2019-06-02 21:23:53 +0200

    [src] add wrapper between UI and Core library

--- src/common/wrapper.cc
+++ src/common/wrapper.cc
@@ -7,25 +7,75 @@
 #include "wrapper.h"
 
 sgx_enclave_id_t global_eid = 0;
-
-int initialize_enclave (void)
+bool authenticated = false;
+wrapper_status_t
+initialize_enclave (void)
 {
-  sgx_status_t rv = SGX_ERROR_UNEXPECTED;
-
-  rv = sgx_create_enclave("../lib/enclave.so", 1, NULL, NULL, &global_eid, NULL);
-  if (rv == SGX_SUCCESS) {
-    return 0;
+  if (SGX_SUCCESS != sgx_create_enclave("../lib/enclave.so", 1, NULL, NULL, &global_eid, NULL)) {
+    return WRAP_ER_INIT;
   }
-  rv = sgx_create_enclave("/usr/share/libsecpass/enclave.so", 1, NULL, NULL, &global_eid, NULL);
-  if (rv != SGX_SUCCESS) {
-    return -1;
-  }
-  return 0;
+  return WRAP_OK;
 }
-int destroy_enclave (void)
+
+wrapper_status_t
+destroy_enclave (void)
 {
   if (SGX_SUCCESS != sgx_destroy_enclave (global_eid)) {
-    return -1;
+    return WRAP_ER_DESTROY;
   }
-  return 0;
+  return WRAP_OK;
+}
+
+
+wrapper_status_t
+secret_add (string name, string secret)
+{
+  uint64_t rv;
+  wrapper_status_t status = WRAP_OK;
+  if (!secret_check (name)) {
+    return WRAP_ER_UNDEF;
+  }
+
+  const char *str = name.c_str ();
+  char       *sec = new char[secret.length () + 1];
+  strcpy (sec, secret.c_str ());
+
+  if (SGX_SUCCESS != entry_add (global_eid, &rv, str, sec)) {
+    return WRAP_ER_ADD;
+  }
+
+  delete[] sec;
+  return status;
+}
+
+wrapper_status_t
+secret_set (char *name, size_t name_len, char *secret, size_t secret_len)
+{
+  wrapper_status_t status = WRAP_OK;
+
+  return status;
+}
+
+wrapper_status_t
+secret_del (string name)
+{
+  uint64_t rv;
+  wrapper_status_t status = WRAP_OK;
+
+  const char *str = name.c_str ();
+  if (SGX_SUCCESS != entry_del (global_eid, &rv, str)) {
+    return WRAP_ER_DEL;
+  }
+
+  return status;
+}
+
+int64_t
+secret_check (string name) {
+  int64_t rv;
+  const char *str = name.c_str ();
+  if (SGX_SUCCESS == entry_check (global_eid, &rv, str)) {
+    return rv;
+  }
+  return -1;
 }
--- src/common/wrapper.h
+++ src/common/wrapper.h
@@ -9,15 +9,59 @@
 #include <cstdint>
 #include <string>
 #include "../core/core_u.h"
+#include "../core/core.h"
 
 #include "sgx_eid.h"
 #include "sgx_urts.h"
 
-int initialize_enclave (void);
-int destroy_enclave (void);
+// version_t: variable determining secpass file format
+typedef uint8_t version_t;
 
-int secret_add (char *name, size_t name_len, char *secret, size_t secret_len);
-int secret_set (char *name, size_t name_len, char *secret, size_t secret_len);
-int secret_del (char *name, size_t name_len);
+// kdf_t: variable determining KDF function used
+typedef uint8_t kdf_t;
+#define KDF_ARGON2    0x00
+#define KDF_RESERVED  0xFF
+
+// enc_t: enum determining encryption function used
+typedef uint8_t enc_t;
+#define ENC_AES128GCM 0x00
+#define ENC_RESERVED  0xFF
+
+// wrapper_status_t: 64 bit variable containing error code
+typedef uint64_t wrapper_status_t;
+
+// definitions of error codes for core_status_t
+#define WRAP_OK          0x00
+#define WRAP_ER_INIT     0x01
+#define WRAP_ER_DESTROY  0x02
+#define WRAP_ER_ADD      0x03
+#define WRAP_ER_DEL      0x04
+#define WRAP_ER_UNDEF    0xFF
+
+wrapper_status_t
+initialize_enclave (void);
+
+wrapper_status_t
+destroy_enclave (void);
+
+wrapper_status_t
+file_auth   (string key, string filepath);
+
+wrapper_status_t
+file_deauth (void);
+
+wrapper_status_t
+secret_add (string name, string secret);
+
+wrapper_status_t
+secret_set (char *name, size_t name_len, char *secret, size_t secret_len);
+
+wrapper_status_t
+secret_del (string name);
+
+int64_t
+secret_check (string name);
+//wrapper_status_t
+//secret_list (std::vector<std::string> *vector);
 
 #endif // _SECPASS_COMMON_WRAPPER_H
